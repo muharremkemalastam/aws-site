e3cd5d3358f3ace448921e4d84067af73c2cf66e
muharremkemalastam <kemalastam@gmail.com>
2026-05-24 15:32:34 +0200

    Added responsive design and hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function showMessage() {
   alert("Welcome to Kemal's Developer Journey!");
 }
+function toggleMenu() {
+  const menu = document.getElementById("navLinks");
+  menu.classList.toggle("active");
+}
